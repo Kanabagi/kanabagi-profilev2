6ec149a3c117d1b314c8280a8a879f5e8c78e1d7
Rifky Alfarez <207006052@student.unsil.ac.id>
2024-04-14 11:42:22 +0700

    nambahin tanda seru biar seru

--- app/layout.tsx
+++ app/layout.tsx
@@ -22,7 +22,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className="scroll-smooth">
+    <html lang="en" className="!scroll-smooth">
       <body className={`${poppins.variable}`}>
         <Navbar />
         {children}
